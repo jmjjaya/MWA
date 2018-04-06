e6930c859c536d8bf4fb3aa19700e9e35d3a7b91
Jeewan Jayathilake <efacjeewan@gmail.com>
2018-04-05 23:09:44 -0500

    lab 04

--- Week1 Labs/Lab04/e02/obser.js
+++ Week1 Labs/Lab04/e02/obser.js
@@ -1,5 +1,19 @@
 const Rx = require('@reactivex/rxjs');
+var os = require('os');
 
-var myObservable = Rx.Subject();
+var myObservable = new Rx.Subject();
 myObservable.subscribe(value => console.log(value));
-myObservable.next("jewan");
+myObservable.next("Checking Your System...");
+const cpu = os.cpus().length;
+const mem = os.totalmem() / 1073741824;
+
+if (cpu >= 2 && mem >= 2) {
+    myObservable.next("System is checked successfully");
+} else {
+    if (mem < 2) {
+        myObservable.next("This app at least needs 2 GB of RAM");
+    }
+    if (cpu < 2) {
+        myObservable.next("Processor is not supported");
+    }
+}
